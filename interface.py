e2e7871e32d3137f9c8cd5d653c80b452cbde0a8
Anton Vashenko <vashchenko-96@mail.ru>
2022-10-19 16:12:43 +0300

    Add exit and return menu in interface

--- interface.py
+++ interface.py
@@ -6,7 +6,8 @@ import logger as l
 
 
 def for_empls():
-    print("Введите 1 для добавления сотрудника\nВведите 2 для удаления сотрудника")
+    print("Введите 1 для добавления сотрудника\nВведите 2 для удаления сотрудника\n\
+    Введите 'q' для выхода\nВведите 'r' для возврата в предыдущее меню")
     choice = input("Ввод: ")
     print()
     if choice == '1':
@@ -15,21 +16,28 @@ def for_empls():
         entity = name, post
         w.add_empl(entity)
         l.log(f"Добавлен сотрудник {name}")
+        exit()
     elif choice == '2':
         r.show_staff()
         id_empl = input("Введите ID сотрудника: ")
         if e.check_id_empl(id_empl):
             w.del_empl(id_empl)
             l.log(f"Удален сотрудник {r.get_name(id_empl)} с ID {id_empl}")
+            exit()
         else:
             print("Такого сотрудника нет\n")
+    elif choice == 'q':
+        exit()
+    elif choice == 'r':
+        return
     else:
         print("Некорректный ввод\n")
 
 
 def for_tables():
     while True:
-        print("Введите 1 для добавления столика\nВведите 2 для удаления столика")
+        print("Введите 1 для добавления столика\nВведите 2 для удаления столика\n\
+        Введите 'q' для выхода\nВведите 'r' для возврата в предыдущее меню")
         choice = input("Ввод: ")
         print()
         if choice == '1':
@@ -37,6 +45,7 @@ def for_tables():
             if not e.check_table(num):
                 w.add_table(num)
                 l.log(f"Добавлен столик с номером {num}")
+                exit()
             else:
                 print("Такой столик уже есть\n")
         elif choice == '2':
@@ -45,23 +54,31 @@ def for_tables():
             if e.check_id_table(id_table):
                 w.del_table(id_table)
                 l.log(f"Удален столик {r.get_number(id_table)} с ID {id_table}")
+                exit()
             else:
                 print("Такого столика нет\n")
+        elif choice == 'q':
+            exit()
+        elif choice == 'r':
+            return
         else:
             print("Некорректный ввод\n")
 
 
 def for_staff():
     while True:
-        print("Введите 1 для работы со списком сотрудников\nВведите 2 для работы со списком столиков")
+        print(f"Введите 1 для работы со списком сотрудников\nВведите 2 для работы со списком столиков\n\
+        Введите 'q' для выхода\nВведите 'r' для возврата в предыдущее меню")
         choice = input("Ввод: ")
         print()
         if choice == '1':
             for_empls()
-            break
         elif choice == '2':
             for_tables()
-            break
+        elif choice == 'q':
+            exit()
+        elif choice == 'r':
+            return
         else:
             print("Некорректный ввод\n")
 
@@ -76,7 +93,7 @@ def for_deals():
             d.add_deal(id_empl, id_table)
             l.log(f"Сотрудник {r.get_name(id_empl)} с ID {id_empl} обслужил\
              столик номер {r.get_number(id_table)} с ID {id_table}")
-            break
+            return
         else:
             if e.check_id_empl(id_empl):
                 r.show_tables_of_empl(id_empl)
@@ -87,14 +104,15 @@ def for_deals():
 
 def work_mode():
     while True:
-        print("Введите 1 для работы с персоналом\nВведите 2 для заключения сделки")
+        print("Введите 1 для работы с персоналом\nВведите 2 для заключения сделки\n\
+        Введите 'q' для выхода")
         choice = input("Ввод: ")
         print()
         if choice == '1':
             for_staff()
-            break
         elif choice == '2':
             for_deals()
-            break
+        elif choice == 'q':
+            exit()
         else:
             print("Некорректный ввод\n")
